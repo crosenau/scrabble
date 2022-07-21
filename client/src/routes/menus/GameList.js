e418d1824a86f3c6dce3fe729c3772fb089ed668
Chris <rosenauc74@hotmail.com>
2022-07-20 17:39:58 -0700

    Added status to game list

--- client/src/routes/menus/GameList.js
+++ client/src/routes/menus/GameList.js
@@ -3,24 +3,44 @@ import GreenButton from '../../components/GreenButton';
 
 export default function GameList({ games, buttonLabel }) {
   const navigate = useNavigate();
+  const formattedGames = games.map(game => {
+    const { id, name } = game;
+    const playerSlots = `${game.players.filter(player => player.userId !== null).length}/${game.players.length}`;
+    const playerIdx = game.turns % game.players.length;
+    const prevPlayerIdx = (game.turns - 1) % game.players.length;
+    let status;
+
+    if (game.gameOver) {
+      status = `${game.players[prevPlayerIdx].userName} won!`;
+    } else if (game.players[playerIdx].userId === null) {
+      status = `Waiting for player to join`;
+    } else {
+      status = `${game.players[playerIdx].userName}'s Turn`;
+    }
+
+    return {
+      id,
+      name,
+      playerSlots,
+      status
+    }
+  });
 
   return (
     <div className="game-list">
       <div className="game-list__headers">
         <div>Name</div>
         <div>Players</div>
-        <div>Last Move</div>
+        <div>Status</div>
         <div></div>
         <hr />
       </div>
       <div className="game-list__rows">
-        {games.map((game, i) => (
-          <div className="game-list__row" key={i}>
+        {formattedGames.map(game => (
+          <div className="game-list__row" key={game.id}>
             <div>{game.name}</div>
-            <div>
-              {`${game.players.filter(player => player.userId !== null).length}/${game.players.length}`}
-            </div>
-            <div>{game.lastActivity || '?'}</div>
+            <div>{game.playerSlots}</div>
+            <div>{game.status}</div>
             <GreenButton
               label={buttonLabel}
               type="button" 
@@ -30,7 +50,6 @@ export default function GameList({ games, buttonLabel }) {
           </div>
         ))}
       </div>
-
     </div>
   );
 }
